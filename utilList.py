8d7e2c6ef3b78b38104ece3af2556195f35230a9
Kit <82869981+Kitrinos78@users.noreply.github.com>
2021-05-31 15:06:40 +0700

    Create utilList.py

---
+++ utilList.py
@@ -0,0 +1,9 @@
+
+timetable = [
+    "Monday:\nENE-201: 12: 00 - 13: 30 @ E302\nJPE-201: 13: 45 - 15: 15 @ E402",
+    "Tuesday:\nDGE-203: 10:15 - 12:15 @ E404\nDGE-203: 13:45 - 17:00 @ Lab",
+    "Wednesday:\nDGE-204: 09:00 - 12:00 @ E401\nENE-201: 12:00 - 13:30 @ E302\nJPE-201: 13:45 - 15:15 @ E402",
+    "Thursday:\nDGE-201: 09:30 - 12:30 @ E404",
+    "Friday:\nDGE-202: 10:15 - 12:15 @ Lab\nDGE-202: 13:45 - 17:00 @ TBA",
+    "No class on this day, you are free... for now"
+]
