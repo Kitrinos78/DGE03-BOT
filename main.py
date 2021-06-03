b3153253b037f752297b2c3efc9ac48fcc696951
Kit <82869981+Kitrinos78@users.noreply.github.com>
2021-06-03 16:12:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -61,16 +61,33 @@ async def on_message(message):
     if msg.startswith(':3 help'):
         await message.channel.send("""
       These are the currently available functions:
-      Main functions:
-      - :3 help [View what the bot can do]
-      - :3 [Calls the bot and shit]
-      - :3 edgy [Say shit from Miles Edgeworth]
-      Timetables:
-      - :3 tt [View timetable this semester]
-      - :3 tt today [View today's timetable]
-      - :3 tt *day* [View that day's timetable]
-      Others:
-      - Try to piss me off with :3 fuck you
+      **---------- Main functions ----------**
+      **:3**  Calls the bot and shit
+      **:3 edgy**  Say shit from Miles Edgeworth
+      **---------- Look-up functions ----------**
+      **:3 define *word***  find definition
+      **:3 urban *word***  find in Urban dictionary
+      **---------- Timetables ----------**
+      **:3 tt**  View timetable this semester
+      **:3 tt today**  View today's timetable
+      **:3 tt *day***  View that day's timetable
+      **---------- Others ----------**
+      **:3 help**  View what the bot can do
+      **:3 credits** View my creator
+      Try to piss me off with **:3 fuck *something***
+        """)
+
+    if msg.startswith(':3 credits'):
+        await message.channel.send("""
+        **CREDITS**
+        > Hey there, pal! 
+        > sorry for the excessive and unnecessary profanities haha they are -uhmm.. totally.. intentional... HOWEVER!!!!
+        > **I am a bot created by <@398269585637507074>**
+        > I am created to be a bot for my creator's friend group, I can do a lot of things (not really, but...still), I can say words your parent said you can't, I can tell you your timetable, I can look up definitions and other fun stuff!
+        > So, just call me when you need me or whatevs ;)
+        > Oh right, here's my repo, it's me in my code form:
+        > https://github.com/Kitrinos78/DGE03-BOT
+        > gotta warn you pal, my code's a fucking mess
         """)
 
     if msg.startswith(':3 fuck me'):
